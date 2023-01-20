2ea80afa8a5ff121c5a054bc88aa065e7f32a410
InFluX <matinazami2003@gmail.com>
2023-01-20 03:30:39 +0330

    Fix Bugs

--- Linkedin/src/main/java/com/example/Linkedin/Main.java
+++ Linkedin/src/main/java/com/example/Linkedin/Main.java
@@ -33,70 +33,20 @@ public class Main {
         }
 
 
-        Graph graph = new Graph();
-
-        for(int i = 0; i < 2000; i++){
-            UserUtil us = list.get(i);
-            User user = new User(us.getId(), us.getName(), us.getEmail(), us.getField(), us.getWorkplace(), us.getUniversityLocation(), us.getDateOfBirth(), us.getSpecialties(), us.getConnectionId());
-            graph.insertVertex(user);
-        }
-
-        for(Vertex vertex : graph.vertices()) {
-            for(String connectionID : vertex.getElement().getConnectionId())
-            {
-                graph.insertEdge(vertex, graph.getVertex(connectionID), vertex.getElement().getId() + "-" + connectionID);
-            }
-        }
-
-//        graph.printBeautified();
-//        System.out.println("--------------");
-//        graph.bfsLevels(graph.getVertex("A"), 8);
-
-//        graph.identifyComponentsDFS();
-//        for (HashSet<Vertex> s : graph.getComponents().values()){
-//            for (Vertex v : s) {
-//                System.out.println(v.getElement());
-//            }
-//            System.out.println("--------------------------------");
+//        Graph graph = new Graph();
+//
+//        for(int i = 0; i < 2000; i++){
+//            UserUtil us = list.get(i);
+//            User user = new User(us.getId(), us.getName(), us.getEmail(), us.getField(), us.getWorkplace(), us.getUniversityLocation(), us.getDateOfBirth(), us.getSpecialties(), us.getConnectionId());
+//            graph.insertVertex(user);
 //        }
-//        System.out.println("sal");
-//        System.out.println(graph.bfsLevels(graph.getVertex("A"), 8));
+//
+//        for(Vertex vertex : graph.vertices()) {
+//            for(String connectionID : vertex.getElement().getConnectionId())
+//            {
+//                graph.insertEdge(vertex, graph.getVertex(connectionID), vertex.getElement().getId() + "-" + connectionID);
+//            }
+//        }
 
-        Centrality c = new Centrality(graph);
-        for(ArrayList<Node> n :  c.degreeCentrality())
-        {
-            for(Node node : n){
-                System.out.println(node.vertex.getElement() + " " + node.value + " " + node.vertex.getEdges().size());
-            }
-            System.out.println("--------------------------------");
-        }
-        System.out.println("SAlaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-
-
-        for(ArrayList<Node> n :  c.closenessCentrality())
-        {
-            for(Node node : n){
-                System.out.println(node.vertex.getElement() + " " + node.value + " " + node.vertex.getEdges().size());
-            }
-            System.out.println("--------------------------------");
-        }
-        System.out.println("SAlaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        for(ArrayList<Node> n :  c.betweennessCentrality())
-        {
-            for(Node node : n){
-                System.out.println(node.vertex.getElement() + " " + node.value + " " + node.vertex.getEdges().size());
-            }
-            System.out.println("--------------------------------");
-        }
-
-        System.out.println("SAlaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        for(ArrayList<Node> n :  c.katzCentrality(0.1))
-        {
-            for(Node node : n){
-                System.out.println(node.vertex.getElement() + " " + node.value + " " + node.vertex.getEdges().size());
-            }
-            System.out.println("--------------------------------");
-        }
-        System.out.println("HHIi");
     }
 }
