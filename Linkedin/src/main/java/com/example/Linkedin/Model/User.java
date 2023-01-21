8f09379a3b7ea7624db7176933742c962a378d76
InFluX <matinazami2003@gmail.com>
2023-01-21 14:38:33 +0330

    ADD FileService & File FileController and Fix Bugs

--- Linkedin/src/main/java/com/example/Linkedin/Model/User.java
+++ Linkedin/src/main/java/com/example/Linkedin/Model/User.java
@@ -19,8 +19,7 @@ import java.util.Set;
 @Table
 public class User {
     @Id
-    @GeneratedValue
-    private Integer id;
+    private String id;
     private String name;
     private String username;
     private String email;
