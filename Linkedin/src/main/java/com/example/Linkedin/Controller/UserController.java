ecbcaca8ea8f8bf152245994fded9a6319a08e47
Zahra <zahraamasoumi@gmail.com>
2023-01-21 18:33:33 +0330

    create all users page

--- Linkedin/src/main/java/com/example/Linkedin/Controller/UserController.java
+++ Linkedin/src/main/java/com/example/Linkedin/Controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.Linkedin.Controller;
 import com.example.Linkedin.Service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/all")
+    public String allUsersPage(Model model) {
+        model.addAttribute("list", userService.getAllUsers());
+        return "all-users";
+    }
 }
